da4daf26a04fb5fe0c586ddec2d7568f31169c0e
lobthere <lolool.le.vrai@gmail.com>
2023-03-24 09:02:52 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+import pygame
 
+pygame.init()
+
+DISPLAY_X = 1600
+DISPLAY_Y = 900
+
+color_background_screen = (255, 255, 255)
+surface = pygame.display.set_mode((DISPLAY_X, DISPLAY_Y))
+pygame.display.set_caption("jeu de conversion")
+surface.fill(color_background_screen)
+pygame.display.flip()
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+pygame.display.update()
