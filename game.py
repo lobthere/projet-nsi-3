dd553687a853cb9cfd74ce23a85d20b7e72988be
lobthere <lobthere@protonmail.com>
2023-04-10 23:02:05 -0400

    ajout de commentaires

--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 import pygame, pyscroll, pytmx
 from pytmx.util_pygame import load_pygame
-import player
+from player import start
 
 def initialisation(display_x, display_y, color_background_screen) :
     """
@@ -10,33 +10,34 @@ def initialisation(display_x, display_y, color_background_screen) :
     COLOR_BACKGROUND_SCREEN: couleur du fond de la fenetre (3 valeurs en code rgb)
     """
 
+
     surface = pygame.display.set_mode((display_x, display_y))   #genere la fenetre
     pygame.display.set_caption("jeu de conversion")             #met le nom de la fenetre
 
-    surface.fill(color_background_screen)       #applique la couleur predefinie a la fenetre
+    surface.fill(color_background_screen)                       #applique la couleur predefinie a la fenetre
     
-    pygame.display.flip()                       #met a jour sur la fenetre
+    pygame.display.flip()                                       #met a jour sur la fenetre
 
-    taille_map = surface.get_size()
-    tmx_data = load_pygame('map\map1.tmx')
-    map_data = pyscroll.data.TiledMapData(tmx_data)
-    map_layer = pyscroll.orthographic.BufferedRenderer(map_data, taille_map)
-    map_layer.zoom = 0.1
+    taille_map = surface.get_size()                             #assigne la taille de la map
+    tmx_data = load_pygame('map\map1.tmx')                      #charge la premiere carte
+    map_data = pyscroll.data.TiledMapData(tmx_data)             
+    map_layer = pyscroll.orthographic.BufferedRenderer(map_data, taille_map)            #
+    map_layer.zoom = 0.1                                                                #applique un dezoom sur le monde
 
-    #player = player.start()
+    player = start()
 
-    group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=1)
+    group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=1)                #cree la carte et lui assigne un plan
+    group.add(player)
 
-    var_return = [surface, group]
-    return var_return
+    return [surface, group]         #retourne plusieurs variables sous la forme d une liste
 
 
 def run(surface, map_layer):
     running = True                              #definie l'etat du programme
 
     while running:
-        map_layer.draw(surface)
-        pygame.display.flip()
+        map_layer.draw(surface)                 #applique la carte sur l'ecran (surface)
+        pygame.display.flip()                   #met a jout
 
         for event in pygame.event.get():        #recupere tout les evenements present sur la fenetre
             if event.type == pygame.QUIT:       #permet de fermer le programme si on clic sur la croix
--- player.py
+++ player.py
@@ -1,13 +1,13 @@
 import pygame
 
-self = pygame.sprite.Sprite
+image_test = pygame.sprite.Sprite
 
 def start():
-    self.sprite_sheet = pygame.image.load("imports/BlueWizard/2BlueWizardIdle/spritesheet.png")
-    self.image = get_image(self, 0, 0)
-    self.rect = self.image.get_rect()
-
-    def get_image(self, x, y):
+    def get_image(_image_test, x, y):
         image = pygame.Surface([32, 32])
-        image.blit(self.sprite_sheet, (0, 0), (x, y, 32, 32))
+        image.blit(_image_test.sprite_sheet, (0, 0), (x, y, 32, 32))
         return image
+
+    image_test.sprite_sheet = pygame.image.load("imports/BlueWizard/2BlueWizardIdle/spritesheet.png")
+    image_test.image = get_image(image_test, 0, 0)
+    image_test.rect = image_test.image.get_rect()
