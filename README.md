e20c41f7fdff69a536163437181da0909b1a1ee6
computerHighSchool <computerHighSchool@thismaildoesnotexist.com>
2023-05-09 15:08:07 -0400

    b

--- README.md
+++
@@ -1,2 +0,0 @@
-# projet-nsi-3
-Nous avons dans le cadre des trophées de la NSI crée ce jeu afin de 
--- Tiles/Mossy - TileSet.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="Mossy - TileSet" tilewidth="512" tileheight="512" tilecount="49" columns="7">
- <image source="../imports/Mossy Tileset/Mossy - TileSet.png" width="3584" height="3584"/>
-</tileset>
