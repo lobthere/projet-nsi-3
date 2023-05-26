8fc6e10a47bb331a0cfd6e6e491bd6ec51d22342
computerHighSchool <computerHighSchool@thismaildoesnotexist.com>
2023-05-26 09:25:36 -0400

    update

---
+++ windows.py
@@ -0,0 +1,38 @@
+from tkinter import *
+
+_width = 500
+_height = 500
+color = 'gray'
+
+
+"""___Le debut___"""
+def t():
+    surface.destroy
+
+surface = Tk()
+surface.title("Un jeu de memoire")
+surface.configure(width=_width, height=_height, bg=color)
+surface.geometry(f"{_width}x{_height}")
+
+"""___le texte___"""
+text_1 = Label(surface, text="Acceuil", bg=color, font=("Arial", 25))
+text_1.place(anchor=CENTER, relx=.48, rely=.2)
+
+"""__button___"""
+b_arithmetique = Button(surface, text="arithmetique", command=None)
+b_arithmetique.place(relx= .2, rely=.5)
+
+b_conversion = Button(surface, text="conversion", command=t())
+b_conversion.place(relx=.45, rely=.5)
+
+b_logique = Button(surface, text="logique", command=None)
+b_logique.place(relx=.7, rely=.5)
+
+
+
+surface.mainloop()
+
+surface = Tk()
+surface.title("Conversion")
+surface.configure(width=200, height=200, bg='red')
+surface.mainloop()
