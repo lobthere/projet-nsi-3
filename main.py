04a94e70531570fb50244e87da0d6e013264944c
lobthere <lobthere@protonmail.com>
2023-05-29 13:45:38 -0400

    Merge branch 'lobthere' of https://github.com/lobthere/projet-nsi-3 into lobthere

--- main.py
+++ main.py
@@ -1,84 +1,9 @@
-import conversion
-from time import sleep
-from threading import Thread
+from tkinter import *
+from tkinter import font, messagebox
+import conversion, PIL
+from PIL import ImageTk, Image
 
+surface = Tk()
+surface.title("Jeu de conversion")
 
-print("bonjour, bienvenue sur le programmme d'entrainenement au calcule mental")    #on dit bonjour au joueur
-
-player_name = input("Choisit ton nom joueur : ")                                    #on retient son nom
-print(f"Bonjour {player_name}")
-pts = 0
-
-def timer(_time: int):
-    for i in range(0, _time, 1):
-        sleep(1)
-    
-
-while True:
-        print("merci de selectionner votre programme : ")                                   #on l invite a selectionner le programme
-        print("1- binaire hexadecimal decimal")
-        print("2- booleens")
-        print("3- arithmetique")
-        print("4- fin du programme")
-        possible_responce = ['1', '2', '3', '4']
-        possible_responce_2 = ['1', '2', '3']
-        programme = input("")
-        if programme not in possible_responce:
-            print("cette option n'est pas disponible")
-        else :
-            if programme == '1':
-                _while = True
-                responce = None
-                while responce not in possible_responce_2:
-                    print('choisit ta difficultee entre 1 et 3')
-                    responce = input()
-                    if responce == '1':
-                        while _while:
-                            r_c = conversion.conversion(10, player_name)
-                            if r_c == 1:
-                                pts = pts + r_c
-                            print(f"votre nombre de points est de {pts}")
-                            print('voulez vous continuez ? y/n')
-                            r = input()
-                            if r =='y':
-                                None
-                            elif r == 'n':
-                                break
-                            else :
-                                break
-                    elif responce == '2':
-                         while _while:
-                            r_c = conversion.conversion(100, player_name)
-                            if r_c == 1:
-                                pts = pts + r_c
-                            print(f"votre nombre de points est de {pts}")
-                            print('voulez vous continuez ? y/n')
-                            r = input()
-                            if r =='y':
-                                None
-                            elif r == 'n':
-                                break
-                            else:
-                                break
-                    elif responce == '3':
-                        while _while:
-                            r_c = conversion.conversion(1000, player_name)
-                            if r_c == 1:
-                                pts = pts + r_c
-                            print(f"votre nombre de points est de {pts}")
-                            print('voulez vous continuez ? y/n')
-                            r = input()
-                            if r =='y':
-                                None
-                            elif r == 'n':
-                                break
-                            else:
-                                break
-            elif programme == '2':
-                print("ce n'est pas disponible pour l instant")
-            elif programme == '3':
-                print("ce n'est pas disponible pour l instant")
-            elif programme == '4':
-                print("fin du programme")
-                break
-leader_board =  None
+surface.mainloop()
