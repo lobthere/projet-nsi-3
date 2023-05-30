88c98eaa716afd5a5f8bac123d66e6bb34b20d09
computerHighSchool <computerHighSchool@thismaildoesnotexist.com>
2023-05-30 16:58:40 -0400

    update

--- windows.py
+++ windows.py
@@ -2,6 +2,8 @@ from tkinter import *
 from PIL import Image, ImageTk as tk
 from conversion_windows import *
 from calcul_logique_tkt_mon_reuf import *
+import json
+
 
 _width = 500
 _height = 500
@@ -31,14 +33,37 @@ surface.geometry(f"{_width}x{_height}")
 text_1 = Label(surface, text="Acceuil", bg=color, font=("Arial", 25))
 text_1.place(anchor=CENTER, relx=.48, rely=.2)
 
+"""__base pour les pseudos et les scores___"""
+
+f_player_temp = open('player.json')
+f_player_data = json.load(f_player_temp)
+
+"""___les def___"""
+def player_button():
+    player_username = player.get()
+    print(player_username)
+    b_logique.place(relx=.7, rely=.5)
+    b_arithmetique.place(relx= .2, rely=.5)
+    b_conversion.place(relx=.45, rely=.5)
+    button_for_player.place(relx=25, rely=25)
+    txt_for_player.place(relx=25, rely=25)
+    player.place(relx=25, rely=25)
+
+
 """__button___"""
 b_arithmetique = Button(surface, text="arithmetique", command=None)
-b_arithmetique.place(relx= .2, rely=.5)
 
 b_conversion = Button(surface, text="conversion", command=conversion)
-b_conversion.place(relx=.45, rely=.5)
 
 b_logique = Button(surface, text="logique", command=logique)
-b_logique.place(relx=.7, rely=.5)
+
+player = Entry(surface, width=15, font=('Arial', 15))
+player.place(relx=.33, rely=.7)
+
+txt_for_player = Label(surface, text='Entre ton nom étranger', bg=color, font=('Arial', 15))
+txt_for_player.place(relx=.3, rely=.6)
+
+button_for_player = Button(surface, text='Entrer', command=player_button)
+button_for_player.place(relx=.7, rely=.7)
 
 surface.mainloop()
